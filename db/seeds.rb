d592e884c77f0abddb3e91d5bf7f48f3cb68bf3d
AustinBH <harlowaustin2@gmail.com>
2019-07-09 11:49:03 -0700

    added pokeapi seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,37 +7,44 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 image_url = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
 
-counter = 1
-while counter <= 50 do
-  name = Faker::Games::Pokemon.name
+counter = 101
+while counter <= 151 do
+  species_request = PokeApi.get(pokemon_species: counter)
+  pokemon_request = PokeApi.get(pokemon: counter)
+  name = species_request.name.capitalize
   pokedex_number = counter
-  description = Faker::ChuckNorris.fact
-  pokemon_type = Faker::Games::Pokemon.move
-  height = Faker::Number.number(2)
-  weight = Faker::Number.number(3)
+  description = species_request.flavor_text_entries[1].flavor_text
+  pokemon_type = ''
+  PokeApi.get(pokemon: counter).types.each do |type_collection|
+    pokemon_type << type_collection.type.name
+  end
+  height = pokemon_request.height.to_i
+  weight = pokemon_request.weight.to_i
   if counter <= 9
     img_url =  "#{image_url}00#{counter}.png"
-  else
+  elsif counter <= 99
     img_url = "#{image_url}0#{counter}.png"
+  else
+    img_url = "#{image_url}#{counter}.png"
   end
 
   Pokemon.create(name: name, pokedex_number: pokedex_number, description: description, pokemon_type: pokemon_type, height: height, weight: weight, img_url: img_url)
   counter += 1
 end
 
-5.times do
-  username = Faker::Name.first_name
-  Trainer.create(username: username)
-end
-
-10.times do
-  name = Faker::JapaneseMedia::DragonBall.character
-  trainer = Trainer.all.sample
-  Team.create(name: name, trainer: trainer)
-end
-
-30.times do
-  team = Team.all.sample
-  pokemon = Pokemon.all.sample
-  PokemonTeam.create(team: team, pokemon: pokemon)
-end
+# 5.times do
+#   username = Faker::Name.first_name
+#   Trainer.create(username: username)
+# end
+#
+# 10.times do
+#   name = Faker::JapaneseMedia::DragonBall.character
+#   trainer = Trainer.all.sample
+#   Team.create(name: name, trainer: trainer)
+# end
+#
+# 30.times do
+#   team = Team.all.sample
+#   pokemon = Pokemon.all.sample
+#   PokemonTeam.create(team: team, pokemon: pokemon)
+# end
