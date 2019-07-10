640839166cf1c787d90fa990deb7fb5cae5af3e2
AustinBH <harlowaustin2@gmail.com>
2019-07-09 17:12:04 -0700

    added unique name validation for trainers

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -2,4 +2,6 @@ class Trainer < ApplicationRecord
   has_many :teams
   has_many :pokemon_teams, through: :teams
   has_many :pokemon, through: :pokemon_teams
+
+  validates :name, uniqueness: true
 end
