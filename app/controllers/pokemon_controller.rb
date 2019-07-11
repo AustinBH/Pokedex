32942d578720dde1c4aada19df5e657119493991
AustinBH <harlowaustin2@gmail.com>
2019-07-11 09:13:12 -0700

    added name search option

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,17 +2,24 @@ class PokemonController < ApplicationController
   def index
     if params[:type]
       pokemons = Pokemon.all
-      all_pokemon = []
-      pokemons.each do |pokemon|
+      all_pokemon = pokemons.select do |pokemon|
         types = pokemon.pokemon_type.split(" ")
-        if types.include?(params[:type])
-          all_pokemon << pokemon
-        end
+        types.include?(params[:type])
       end
       if all_pokemon.length > 0
         render json: all_pokemon, exclude: [:created_at, :updated_at]
       else
         render json: {message: "There are no pokemon of #{params[:type]} type."}
+      end
+    elsif params[:name]
+      pokemons = Pokemon.all
+      all_pokemon = pokemons.select do |pokemon|
+        pokemon.name.downcase.include?(params[:name])
+      end
+      if all_pokemon.length > 0
+        render json: all_pokemon, exclude: [:created_at, :updated_at]
+      else
+        render json: {message: "There are no pokemon with #{params[:name]} in their name."}
       end
     else
       all_pokemon = Pokemon.all
