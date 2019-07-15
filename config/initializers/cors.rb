7743078f156fd7217ecf7ecba65874a150ba43d5
AustinBH <harlowaustin2@gmail.com>
2019-07-15 00:23:44 -0700

    updating master to allow just two application routes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ require_relative '../environment'
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'austinbh.github.io', 'pokedex-yeet.herokuapp.com', 'localhost:3000'
+    origins 'austinbh.github.io', 'pokedex-yeet.herokuapp.com'
 
     resource '*',
       headers: :any,
