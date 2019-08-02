6a6bb63da818da80d71000dc368d725087e8dcb5
AustinBH <harlowaustin2@gmail.com>
2019-08-02 16:03:50 -0700

    updated cors routes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,6 +14,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: :get
+  end
+
+  allow do
+    origins 'https://austinbh.github.io/Pokedex-frontend/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :put, :patch, :delete, :options, :head]
   end
 end
